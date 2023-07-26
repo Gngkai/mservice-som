3b192fd48a21876491e1aa7413b9bc906da7eb20
023529 <724213138@qq.com>
2023-07-26 08:38:56 +0800

    关键词库

--- mkt/data/point/wordEditForm.java
+++ mkt/data/point/wordEditForm.java
@@ -3,9 +3,9 @@ package mkt.data.point;
 import kd.bos.form.plugin.AbstractFormPlugin;
 
 /**
- * @author create by ?
+ * @author create by gk
  * @date 2023/7/25 20:06
- * @action
+ * @action 子表转入
  */
 public class wordEditForm extends AbstractFormPlugin {
 }
