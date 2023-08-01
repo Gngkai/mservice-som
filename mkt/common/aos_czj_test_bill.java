9e7426983c3ca0253eb5e2616e07ec436f8b74db
023529 <724213138@qq.com>
2023-08-01 14:13:13 +0800

    feat(sal): quote
    成本价格计算器调整
    	-界面和功能移除标准快递费明细计算
    	-多国别批量计算

--- mkt/common/aos_czj_test_bill.java
+++ mkt/common/aos_czj_test_bill.java
@@ -1,18 +1,30 @@
 package mkt.common;
 
-import java.util.EventObject;
-import java.util.StringJoiner;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
 
+import common.sal.util.SalUtil;
+import common.sal.util.SaveUtils;
 import kd.bos.bill.AbstractBillPlugIn;
 import kd.bos.dataentity.entity.DynamicObject;
+import kd.bos.dataentity.entity.DynamicObjectCollection;
+import kd.bos.dataentity.entity.ILocaleString;
 import kd.bos.entity.datamodel.events.PropertyChangedArgs;
 import kd.bos.form.control.Control;
 import kd.bos.form.control.events.ClickListener;
 import kd.bos.form.control.events.ItemClickEvent;
 import kd.bos.form.control.events.ItemClickListener;
+import kd.bos.logging.Log;
+import kd.bos.logging.LogFactory;
+import kd.bos.orm.query.QFilter;
 import kd.bos.servicehelper.BusinessDataServiceHelper;
+import kd.bos.servicehelper.QueryServiceHelper;
 import kd.bos.servicehelper.operation.SaveServiceHelper;
 import kd.fi.bd.util.QFBuilder;
+import kd.scm.pur.opplugin.util.SaveUtil;
 import mkt.synciface.aos_mkt_item_sync;
 
 public class aos_czj_test_bill extends AbstractBillPlugIn implements ItemClickListener, ClickListener {
@@ -23,13 +35,23 @@ public class aos_czj_test_bill extends AbstractBillPlugIn implements ItemClickLi
 		this.addItemClickListeners("aos_toolbarap");
 		this.addItemClickListeners("aos_test");
 	}
-
+	private static Log log = LogFactory.getLog("mkt.common.aos_czj_test_bill");
 	@Override
 	public void itemClick(ItemClickEvent evt) {
 		super.itemClick(evt);
 		String Control = evt.getItemKey();
-		if (Control.equals("aos_test"))
-			aos_test();
+		if (Control.equals("aos_test")){
+
+			log.info("迁移开始： {}"+ LocalDateTime.now().toString());
+			try {
+				keyWordSync();
+			}catch (Exception e){
+				StringWriter sw = new StringWriter();
+				e.printStackTrace(new PrintWriter(sw));
+				log.error(sw.toString());
+			}
+			log.info("迁移结束： {}"+ LocalDateTime.now().toString());
+		}
 	}
 
 	private void aos_test() {
@@ -57,6 +79,57 @@ public class aos_czj_test_bill extends AbstractBillPlugIn implements ItemClickLi
 		SaveServiceHelper.update(aos_mkt_listing_sons);
 		message.add("文案修改完成");
 		this.getView().showMessage(message.toString());
+	}
+
+	private void keyWordSync(){
+		DynamicObjectCollection dyc = QueryServiceHelper.query("aos_czj_tmp", "aos_string1,aos_string2,aos_string3", null);
+		Map<String, List<DynamicObject>> map_data = dyc.stream()
+				.collect(Collectors.groupingBy(dy -> dy.getString("aos_string1")));
+		List<String> list = new ArrayList<>(map_data.keySet());
+		Map<String, String> map_rowData = dyc.stream()
+				.collect(Collectors.toMap(
+						dy -> dy.getString("aos_string2"),
+						dy -> dy.getString("aos_string3"),
+						(old, newvalue) -> old
+				));
+
+
+		QFBuilder builder = new QFBuilder();
+		builder.add("id", QFilter.in,list);
+		StringJoiner str = new StringJoiner(",");
+		str.add("aos_itemnamecn");
+		str.add("id");
+		str.add("aos_linentity.id");
+		str.add("aos_linentity.aos_correlate");
+		List<DynamicObject> update = new ArrayList<>(dyc.size());
+		DynamicObject[] mkt_points = BusinessDataServiceHelper.load("aos_mkt_point", str.toString(), builder.toArray());
+		for (DynamicObject dy : mkt_points) {
+			DynamicObjectCollection aos_linentity = dy.getDynamicObjectCollection("aos_linentity");
+			for (DynamicObject entity : aos_linentity) {
+				String entityid = entity.getPkValue().toString();
+				if (map_rowData.containsKey(entityid)) {
+					String value = map_rowData.get(entityid);
+					ILocaleString correlate = entity.getLocaleString("aos_correlate");
+					correlate.setLocaleValue_zh_CN(value);
+					if (value.contains("高")||value.contains("强")){
+						correlate.setLocaleValue_en("High");
+					}
+					else if (value.contains("中")){
+						correlate.setLocaleValue_en("Medium");
+					}
+					else if (value.contains("低")||value.contains("弱")){
+						correlate.setLocaleValue_en("Low");
+					}
+					else {
+						correlate.setLocaleValue_en(value);
+					}
+					entity.set("aos_correlate",correlate);
+
+				}
+			}
+			update.add(dy);
+		}
+		SaveServiceHelper.save(mkt_points);
 	}
 
 	public void beforePropertyChanged(PropertyChangedArgs e) {
