d5c5cbe3a908168788d5a9b9f2c878574114f475
czj <154661961@qq.com>
2023-10-18 16:39:52 +0800

    单元测试最终版

--- mkt/image/test/aos_mkt_bitest_form.java
+++ mkt/image/test/aos_mkt_bitest_form.java
@@ -1,15 +1,6 @@
 package mkt.image.test;
 
-import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
 import java.util.EventObject;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import common.fnd.AosomLog;
 import common.fnd.FndMsg;
 import kd.bos.form.control.events.ItemClickEvent;
 import kd.bos.form.plugin.AbstractFormPlugin;
@@ -46,7 +37,7 @@ public class aos_mkt_bitest_form extends AbstractFormPlugin {
 	}
 
 	public static Object aos_junit(Object obj) {
-		FndMsg.debug("obj:"+obj);
+		FndMsg.debug("obj:" + obj);
 		return obj;
 	}
 
@@ -62,32 +53,4 @@ public class aos_mkt_bitest_form extends AbstractFormPlugin {
 		return n;
 	}
 
-
-
-	public void error(String var, Object... var2) {
-		String aString = MessageFormat.format(var, var2);
-		FndMsg.debug("aString:" + aString);
-	}
-
-	public static long getSecondsDifference(Date startDate, Date endDate) {
-		long startMillis = startDate.getTime();
-		long endMillis = endDate.getTime();
-		long diffMillis = endMillis - startMillis;
-		return diffMillis / 1000; // 将毫秒转换为秒
-	}
-
-	private static void sortMapValue() {
-		Map<String, String> map = new HashMap<>();
-		map.put("a", "2");
-		map.put("c", "5");
-		map.put("d", "6");
-		map.put("b", "1");
-		List<Map.Entry<String, String>> lstEntry = new ArrayList<>(map.entrySet());
-		Collections.sort(lstEntry, ((o1, o2) -> {
-			return o1.getValue().compareTo(o2.getValue());
-		}));
-		
-		lstEntry.forEach(o -> {
-		});
-	}
 }
