6c783a8aaaa77eb5cf203928ecd7c196c1a657c9
czj <154661961@qq.com>
2023-10-19 08:19:59 +0800

    单元测试最终版

---
+++ mkttest/unit/image/test/aos_mkt_bitest_formTest.java
@@ -0,0 +1,43 @@
+package mktTest.unit.image.test;
+
+import mkt.image.test.aos_mkt_bitest_form;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class aos_mkt_bitest_formTest {
+
+    private mkt.image.test.aos_mkt_bitest_form aos_mkt_bitest_form;
+
+    @BeforeEach
+    public void setUp() {
+        this.aos_mkt_bitest_form = new aos_mkt_bitest_form();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        this.aos_mkt_bitest_form = null;
+    }
+
+    @Test
+    void testAdd() {
+        assertEquals(100, this.aos_mkt_bitest_form.add(100));
+        assertEquals(150, this.aos_mkt_bitest_form.add(50));
+        assertEquals(130, this.aos_mkt_bitest_form.add(-20));
+    }
+
+    @Test
+    void testSub() {
+        assertEquals(-100, this.aos_mkt_bitest_form.sub(100));
+        assertEquals(-150, this.aos_mkt_bitest_form.sub(50));
+        assertEquals(-130, this.aos_mkt_bitest_form.sub(-20));
+    }
+
+    @Test
+    void aos_junit() {
+        assertEquals(1, mkt.image.test.aos_mkt_bitest_form.aos_junit(1));
+        assertEquals(1, mkt.image.test.aos_mkt_bitest_form.aos_junit(1));
+    }
+}
