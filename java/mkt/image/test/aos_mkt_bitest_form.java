4c42d1329031271fff40953d2ad9b7c24abf8dbe
czj <154661961@qq.com>
2023-11-28 19:24:38 +0800

    feat: 拍照需求表新增编辑人员
    拍照需求表新增编辑人员

--- java/mkt/image/test/aos_mkt_bitest_form.java
+++ java/mkt/image/test/aos_mkt_bitest_form.java
@@ -11,7 +11,10 @@ import kd.bos.form.plugin.AbstractFormPlugin;
 import kd.bos.orm.query.QFilter;
 import kd.bos.servicehelper.QueryServiceHelper;
 import mkt.progress.design.aadd.aos_mkt_aadd_init;
+import mkt.progress.listing.manager.ListingManaCateSync;
+import mkt.progress.listing.manager.ListingManaClSync;
 import mkt.progress.listing.manager.ListingManaInitTask;
+import mkt.progress.listing.manager.ListingManaKeyWordSync;
 
 public class aos_mkt_bitest_form extends AbstractFormPlugin {
 
