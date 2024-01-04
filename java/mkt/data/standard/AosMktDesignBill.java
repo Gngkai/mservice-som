66ee683d402ae6d66903ed7f71e7d771d0af150e
czj <154661961@qq.com>
2024-01-04 18:59:12 +0800

    feat(bill): 营销代码调整
    营销代码调整

--- java/mkt/data/standard/AosMktDesignBill.java
+++ java/mkt/data/standard/AosMktDesignBill.java
@@ -2,17 +2,19 @@ package mkt.data.standard;
 
 import common.fnd.FndGlobal;
 import common.fnd.FndMsg;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Scope;
 import kd.bos.bill.AbstractBillPlugIn;
+import kd.bos.context.RequestContext;
 import kd.bos.dataentity.OperateOption;
 import kd.bos.dataentity.entity.DynamicObject;
 import kd.bos.form.ConfirmCallBackListener;
 import kd.bos.form.MessageBoxOptions;
-import kd.bos.form.control.EntryGrid;
 import kd.bos.form.control.events.ItemClickEvent;
 import kd.bos.form.control.events.ItemClickListener;
 import kd.bos.form.events.BeforeDoOperationEventArgs;
 import kd.bos.form.events.MessageBoxClosedEvent;
-import kd.bos.form.field.BasedataEdit;
 import kd.bos.form.operate.FormOperate;
 import kd.bos.orm.query.QCP;
 import kd.bos.orm.query.QFilter;
@@ -20,10 +22,16 @@ import kd.bos.servicehelper.BusinessDataServiceHelper;
 import kd.bos.servicehelper.QueryServiceHelper;
 import kd.bos.servicehelper.operation.SaveServiceHelper;
 import kd.bos.servicehelper.user.UserServiceHelper;
+import mkt.common.otel.MmsOtelUtils;
 
 import java.util.EventObject;
 
+/**
+ * @author aosom
+ */
 public class AosMktDesignBill extends AbstractBillPlugIn implements ItemClickListener {
+    private static final Tracer TRACER = MmsOtelUtils.getTracer(AosMktDesignBill.class, RequestContext.get());
+
     @Override
     public void registerListener(EventObject e) {
         try {
@@ -33,44 +41,37 @@ public class AosMktDesignBill extends AbstractBillPlugIn implements ItemClickLis
         }
     }
 
-    private void aos_manuopen(DynamicObject dy_main, String type) {
+    private void aosManuopen(DynamicObject dyMain) {
         {
-            dy_main.set("aos_status", "待优化");
-            SaveServiceHelper.saveOperate("aos_mkt_designstd",
-                    new DynamicObject[]{dy_main}, OperateOption.create());
-            if (type.equals("A")) {
-                this.getView().invokeOperation("refresh");
-                this.getView().showSuccessNotification("手动开启成功");
-            }
+            dyMain.set("aos_status", "待优化");
+            SaveServiceHelper.saveOperate("aos_mkt_designstd", new DynamicObject[] {dyMain}, OperateOption.create());
+
+            this.getView().invokeOperation("refresh");
+            this.getView().showSuccessNotification("手动开启成功");
+
         }
     }
 
     /**
      * 手工关闭
      *
-     * @param dy_main
-     * @param type
      */
-    private void aos_manuclose(DynamicObject dy_main, String type) {
-        dy_main.set("aos_status", "手动关闭");
-        SaveServiceHelper.saveOperate("aos_mkt_designstd",
-                new DynamicObject[]{dy_main}, OperateOption.create());
-        if (type.equals("A")) {
-            this.getView().invokeOperation("refresh");
-            this.getView().showSuccessNotification("手动关闭成功");
-        }
+    private void aosManuclose(DynamicObject dyMain) {
+        dyMain.set("aos_status", "手动关闭");
+        SaveServiceHelper.saveOperate("aos_mkt_designstd", new DynamicObject[] {dyMain}, OperateOption.create());
+        this.getView().invokeOperation("refresh");
+        this.getView().showSuccessNotification("手动关闭成功");
     }
 
-    private void aos_audit(DynamicObject dy_main, String type) {
-        dy_main.set("aos_status", "已完成");
-        dy_main.set("aos_confirmor", UserServiceHelper.getCurrentUserId());
-        SaveServiceHelper.saveOperate("aos_mkt_designstd",
-                new DynamicObject[]{dy_main}, OperateOption.create());
-        if (type.equals("A")) {
-            this.getView().invokeOperation("refresh");
-            this.getView().showSuccessNotification("审核成功");
-            this.getView().showConfirm("是否调整摄影、摄像、布景标准库?", MessageBoxOptions.YesNo, new ConfirmCallBackListener("audit", this));
-        }
+    private void aosAudit(DynamicObject dyMain) {
+        dyMain.set("aos_status", "已完成");
+        dyMain.set("aos_confirmor", UserServiceHelper.getCurrentUserId());
+        SaveServiceHelper.saveOperate("aos_mkt_designstd", new DynamicObject[] {dyMain}, OperateOption.create());
+        this.getView().invokeOperation("refresh");
+        this.getView().showSuccessNotification("审核成功");
+        this.getView().showConfirm("是否调整摄影、摄像、布景标准库?", MessageBoxOptions.YesNo,
+            new ConfirmCallBackListener("audit", this));
+
     }
 
     @Override
@@ -81,19 +82,17 @@ public class AosMktDesignBill extends AbstractBillPlugIn implements ItemClickLis
         // 确认结果
         String resultValue = messageBoxClosedEvent.getResultValue();
         // 过滤条件
-        String aos_category1_name = this.getModel().getValue("aos_category1_name").toString();
-        String aos_category2_name = this.getModel().getValue("aos_category2_name").toString();
-        String aos_category3_name = this.getModel().getValue("aos_category3_name").toString();
-        String aos_itemnamecn = this.getModel().getValue("aos_itemnamecn").toString();
-        String aos_detail = this.getModel().getValue("aos_detail").toString();
+        String aosCategory1Name = this.getModel().getValue("aos_category1_name").toString();
+        String aosCategory2Name = this.getModel().getValue("aos_category2_name").toString();
+        String aosCategory3Name = this.getModel().getValue("aos_category3_name").toString();
+        String aosItemnamecn = this.getModel().getValue("aos_itemnamecn").toString();
+        String aosDetail = this.getModel().getValue("aos_detail").toString();
 
-        QFilter[] qFilters = new QFilter[]{
-                new QFilter("aos_category1_name", QCP.equals, aos_category1_name),
-                new QFilter("aos_category2_name", QCP.equals, aos_category2_name),
-                new QFilter("aos_category3_name", QCP.equals, aos_category3_name),
-                new QFilter("aos_itemnamecn", QCP.equals, aos_itemnamecn),
-                new QFilter("aos_detail", QCP.equals, aos_detail),
-        };
+        QFilter[] qFilters = new QFilter[] {new QFilter("aos_category1_name", QCP.equals, aosCategory1Name),
+            new QFilter("aos_category2_name", QCP.equals, aosCategory2Name),
+            new QFilter("aos_category3_name", QCP.equals, aosCategory3Name),
+            new QFilter("aos_itemnamecn", QCP.equals, aosItemnamecn),
+            new QFilter("aos_detail", QCP.equals, aosDetail),};
         FndMsg.debug("resultValue:" + resultValue);
         FndMsg.debug("callBackId:" + callBackId);
 
@@ -101,26 +100,29 @@ public class AosMktDesignBill extends AbstractBillPlugIn implements ItemClickLis
             FndMsg.debug("======== into if ========");
 
             // 如果为是 更新相同品类的摄影 摄像 布景标准库 的进度为待优化
-            DynamicObject photoObj = BusinessDataServiceHelper.loadSingle("aos_mkt_photostd", "billstatus,aos_status", qFilters);
-            DynamicObject videoObj = BusinessDataServiceHelper.loadSingle("aos_mkt_videostd", "billstatus,aos_status", qFilters);
-            DynamicObject viewObj = BusinessDataServiceHelper.loadSingle("aos_mkt_viewstd", "billstatus,aos_status", qFilters);
+            DynamicObject photoObj =
+                BusinessDataServiceHelper.loadSingle("aos_mkt_photostd", "billstatus,aos_status", qFilters);
+            DynamicObject videoObj =
+                BusinessDataServiceHelper.loadSingle("aos_mkt_videostd", "billstatus,aos_status", qFilters);
+            DynamicObject viewObj =
+                BusinessDataServiceHelper.loadSingle("aos_mkt_viewstd", "billstatus,aos_status", qFilters);
             if (photoObj != null) {
                 FndMsg.debug("更新拍照需求表");
                 photoObj.set("billstatus", "D");
                 photoObj.set("aos_status", "待优化");
-                SaveServiceHelper.save(new DynamicObject[]{photoObj});
+                SaveServiceHelper.save(new DynamicObject[] {photoObj});
             }
             if (videoObj != null) {
                 FndMsg.debug("更新摄像需求表");
                 videoObj.set("billstatus", "D");
                 videoObj.set("aos_status", "待优化");
-                SaveServiceHelper.save(new DynamicObject[]{videoObj});
+                SaveServiceHelper.save(new DynamicObject[] {videoObj});
             }
             if (viewObj != null) {
                 FndMsg.debug("更新布景需求表");
                 viewObj.set("billstatus", "D");
                 viewObj.set("aos_status", "待优化");
-                SaveServiceHelper.save(new DynamicObject[]{viewObj});
+                SaveServiceHelper.save(new DynamicObject[] {viewObj});
             }
         }
     }
@@ -128,52 +130,56 @@ public class AosMktDesignBill extends AbstractBillPlugIn implements ItemClickLis
     /**
      * 提交按钮
      *
-     * @param dy_main
-     * @param type
      */
-    private void aos_submit(DynamicObject dy_main, String type) {
-        dy_main.set("aos_status", "待确认");
-        SaveServiceHelper.saveOperate("aos_mkt_designstd",
-                new DynamicObject[]{dy_main}, OperateOption.create());
-        if (type.equals("A")) {
-            this.getView().invokeOperation("refresh");
-            this.getView().showSuccessNotification("提交成功");
-        }
+    private void aosSubmit(DynamicObject dyMain) {
+        dyMain.set("aos_status", "待确认");
+        SaveServiceHelper.saveOperate("aos_mkt_designstd", new DynamicObject[] {dyMain}, OperateOption.create());
+        this.getView().invokeOperation("refresh");
+        this.getView().showSuccessNotification("提交成功");
+
     }
 
+    @Override
     public void itemClick(ItemClickEvent evt) {
-        super.itemClick(evt);
-        String Control = evt.getItemKey();
-        if ("aos_submit".equals(Control)) {
-            DynamicObject dy_main = this.getModel().getDataEntity(true);
-            aos_submit(dy_main, "A");// 提交
-        } else if ("aos_audit".equals(Control)) {
-            DynamicObject dy_main = this.getModel().getDataEntity(true);
-            aos_audit(dy_main, "A");// 审核
-        } else if ("aos_manuclose".equals(Control)) {
-            DynamicObject dy_main = this.getModel().getDataEntity(true);
-            aos_manuclose(dy_main, "A");// 手工关闭
-        } else if ("aos_manuopen".equals(Control)) {
-            DynamicObject dy_main = this.getModel().getDataEntity(true);
-            aos_manuopen(dy_main, "A");// 手工开启
+        Span span = MmsOtelUtils.getCusMainSpan(TRACER, MmsOtelUtils.getMethodPath());
+        try (Scope ignored = span.makeCurrent()) {
+            super.itemClick(evt);
+            String control = evt.getItemKey();
+            if ("aos_submit".equals(control)) {
+                DynamicObject dyMain = this.getModel().getDataEntity(true);
+                aosSubmit(dyMain);// 提交
+            } else if ("aos_audit".equals(control)) {
+                DynamicObject dyMain = this.getModel().getDataEntity(true);
+                aosAudit(dyMain);// 审核
+            } else if ("aos_manuclose".equals(control)) {
+                DynamicObject dyMain = this.getModel().getDataEntity(true);
+                aosManuclose(dyMain);// 手工关闭
+            } else if ("aos_manuopen".equals(control)) {
+                DynamicObject dyMain = this.getModel().getDataEntity(true);
+                aosManuopen(dyMain);// 手工开启
+            }
+        } catch (Exception ex) {
+            MmsOtelUtils.setException(span, ex);
+        } finally {
+            MmsOtelUtils.spanClose(span);
         }
     }
 
+    @Override
     public void beforeDoOperation(BeforeDoOperationEventArgs args) {
         super.beforeDoOperation(args);
-        FormOperate formOperate = (FormOperate) args.getSource();
-        String Operatation = formOperate.getOperateKey();
-        if ("save".equals(Operatation)) {
+        FormOperate formOperate = (FormOperate)args.getSource();
+        String operatation = formOperate.getOperateKey();
+        if ("save".equals(operatation)) {
             FndMsg.debug("=====save Operation=====");
             // 校验
             DynamicObject aosMktStandard = QueryServiceHelper.queryOne("aos_mkt_designstd", "id",
-                    new QFilter("aos_category1_name", QCP.equals, this.getModel().getValue("aos_category1_name"))
-                            .and("aos_category2_name", QCP.equals, this.getModel().getValue("aos_category2_name"))
-                            .and("aos_category3_name", QCP.equals, this.getModel().getValue("aos_category3_name"))
-                            .and("aos_itemnamecn", QCP.equals, this.getModel().getValue("aos_itemnamecn"))
-                            .and("aos_detail", QCP.equals, this.getModel().getValue("aos_detail"))
-                            .and("id", QCP.not_equals, this.getModel().getDataEntity().getPkValue().toString())
-                            .toArray());
+                new QFilter("aos_category1_name", QCP.equals, this.getModel().getValue("aos_category1_name"))
+                    .and("aos_category2_name", QCP.equals, this.getModel().getValue("aos_category2_name"))
+                    .and("aos_category3_name", QCP.equals, this.getModel().getValue("aos_category3_name"))
+                    .and("aos_itemnamecn", QCP.equals, this.getModel().getValue("aos_itemnamecn"))
+                    .and("aos_detail", QCP.equals, this.getModel().getValue("aos_detail"))
+                    .and("id", QCP.not_equals, this.getModel().getDataEntity().getPkValue().toString()).toArray());
             if (FndGlobal.IsNotNull(aosMktStandard)) {
                 this.getView().showErrorNotification("已存在相同的产品类别和品名");
                 args.setCancel(true);
---
+++ java/mkt/synciface/AosNewItemPrtInit.java
@@ -0,0 +1,56 @@
+package mkt.synciface;
+
+import kd.bos.context.RequestContext;
+import kd.bos.dataentity.entity.DynamicObject;
+import kd.bos.dataentity.entity.DynamicObjectCollection;
+import kd.bos.exception.KDException;
+import kd.bos.orm.query.QCP;
+import kd.bos.orm.query.QFilter;
+import kd.bos.schedule.executor.AbstractTask;
+import kd.bos.servicehelper.BusinessDataServiceHelper;
+import kd.bos.servicehelper.QueryServiceHelper;
+import kd.bos.servicehelper.operation.DeleteServiceHelper;
+import kd.bos.servicehelper.operation.SaveServiceHelper;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * Date:2024/1/4 14:31
+ * 
+ * @author aosom
+ */
+public class AosNewItemPrtInit extends AbstractTask {
+    @Override
+    public void execute(RequestContext requestContext, Map<String, Object> map) throws KDException {
+        process();
+    }
+
+    private void process() {
+        Calendar today = Calendar.getInstance();
+        today.set(Calendar.HOUR_OF_DAY, 0);
+        today.set(Calendar.MINUTE, 0);
+        today.set(Calendar.SECOND, 0);
+        today.set(Calendar.MILLISECOND, 0);
+        SimpleDateFormat writeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);// 日期格式化
+        String dateStr = writeFormat.format(today.getTime());
+        DeleteServiceHelper.delete("aos_newitem_prt", new QFilter("createtime", QCP.large_equals, dateStr).toArray());
+        DynamicObjectCollection itemS = QueryServiceHelper.query("bd_material",
+            "id itemid," + "aos_contryentry.aos_nationality orgid," + "aos_contryentry.aos_contrybrand brandid,"
+                + "aos_contryentry.aos_firstindate aos_firstindate",
+            new QFilter("aos_type", QCP.equals, "A").and("aos_contryentry.aos_firstindate", QCP.large_equals, dateStr)
+                .toArray());
+        for (DynamicObject item : itemS) {
+            DynamicObject newItem = BusinessDataServiceHelper.newDynamicObject("aos_newitem_prt");
+            newItem.set("aos_orgid", item.getString("orgid"));
+            newItem.set("aos_itemid", item.getString("itemid"));
+            newItem.set("aos_brandid", item.getString("brandid"));
+            newItem.set("aos_rcv_date", item.get("aos_firstindate"));
+            newItem.set("createtime", new Date());
+            SaveServiceHelper.save(new DynamicObject[] {newItem});
+        }
+    }
+}
