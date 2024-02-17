5828e13ff2e2fdc17ae3a551c3d00540ca3fab27
czj <154661961@qq.com>
2024-02-17 08:08:17 +0800

    feat(bill): 代码优化
    代码优化

--- java/mkt/progress/listing/manager/ListingManaInitBill.java
+++ java/mkt/progress/listing/manager/ListingManaInitBill.java
@@ -6,34 +6,43 @@ import kd.bos.entity.datamodel.events.PropertyChangedArgs;
 import java.util.EventObject;
 
 /**
- * @author aosom
- * 数字资产管理表单插件
+ * @author aosom 数字资产管理表单插件
  */
 public class ListingManaInitBill extends AbstractBillPlugIn {
+    public final static String AOS_POINT = "aos_point";
+
+    @Override
     public void propertyChanged(PropertyChangedArgs e) {
         String name = e.getProperty().getName();
-        if (name.contains("aos_point")) {
-            int total = 0;
-            for (int i = 1; i <= 12; i++) {
-                total = total + (Integer) this.getModel().getValue("aos_point" + i, 0);
-            }
-            this.getModel().setValue("aos_total", total, 0);
+        if (name.contains(AOS_POINT)) {
+            pointChanged();
         }
+    }
+
+    private void pointChanged() {
+        int total = 0;
+        int twelve = 12;
+        for (int i = 1; i <= twelve; i++) {
+            total = total + (Integer)this.getModel().getValue("aos_point" + i, 0);
+        }
+        this.getModel().setValue("aos_total", total, 0);
     }
 
     /**
      * 初始化事件
      **/
+    @Override
     public void afterLoadData(EventObject e) {
         super.afterLoadData(e);
-        StatusControl();
+        statusControl();
+        pointChanged();
     }
 
     /**
      * 通用状态控制
      */
-    private void StatusControl() {
-        DynamicObject aosOrgDyn = (DynamicObject) this.getModel().getValue("aos_orgid");
+    private void statusControl() {
+        DynamicObject aosOrgDyn = (DynamicObject)this.getModel().getValue("aos_orgid");
         String aosOrgNum = aosOrgDyn.getString("number");
 
         this.getView().setVisible(false, "aos_us_sale");
@@ -46,6 +55,5 @@ public class ListingManaInitBill extends AbstractBillPlugIn {
 
         this.getView().setVisible(true, "aos_" + aosOrgNum.toLowerCase() + "_sale");
     }
-
 
 }
