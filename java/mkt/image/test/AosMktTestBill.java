449740ef5f7e3cd21313fd7cfc49b192228751c1
czj <154661961@qq.com>
2024-02-17 08:07:44 +0800

    feat(bill): 自定义控件
    自定义控件

--- java/mkt/image/test/AosMktTestBill.java
+++ java/mkt/image/test/AosMktTestBill.java
@@ -1,34 +1,7 @@
 package mkt.image.test;
 
 import kd.bos.bill.AbstractBillPlugIn;
-import kd.bos.dataentity.entity.DynamicObject;
-import kd.bos.dataentity.entity.DynamicObjectCollection;
-import kd.bos.entity.tree.TreeNode;
-import kd.bos.form.control.Html;
-import kd.bos.form.control.TreeView;
 import kd.bos.form.control.events.ItemClickEvent;
-import kd.bos.servicehelper.QueryServiceHelper;
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartUtilities;
-import org.jfree.chart.JFreeChart;
-import org.jfree.data.xy.DefaultXYDataset;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
-
-import java.awt.Color;
-import java.awt.geom.Ellipse2D;
-import org.jfree.chart.plot.PlotOrientation;
-
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
-import java.awt.image.BufferedImage;
-import javax.imageio.ImageIO;
 
 /**
  * @author aosom
@@ -38,157 +11,15 @@ public class AosMktTestBill extends AbstractBillPlugIn {
 
     public final static String AOS_CAL = "aos_cal";
 
-    public static List<String[]> cartesianProduct(List<String[]> arrays) {
-        List<String[]> result = new ArrayList<>();
-        cartesianProductHelper(result, new String[arrays.size()], arrays, 0);
-        return result;
-    }
-
-    private static void cartesianProductHelper(List<String[]> result, String[] current, List<String[]> arrays,
-        int depth) {
-        if (depth == arrays.size()) {
-            result.add(current.clone());
-            return;
-        }
-
-        for (String str : arrays.get(depth)) {
-            current[depth] = str;
-            cartesianProductHelper(result, current, arrays, depth + 1);
-        }
-    }
-
-    private static double[][] generateRandomData(int count) {
-        double[][] data = new double[2][count];
-        Random random = new Random();
-        for (int i = 0; i < count; i++) {
-            double x = random.nextDouble();
-            double y = random.nextDouble();
-            data[0][i] = x;
-            data[1][i] = y;
-        }
-        return data;
-    }
-
     @Override
     public void itemClick(ItemClickEvent evt) {
         super.itemClick(evt);
         String control = evt.getItemKey();
-        try {
-            if (AOS_CAL.equals(control)) {
-                aosCal();
-                aosTest("Keyword Filter0", "Click0", "Expouse0", "aos_htmlap", 800, 600);
-                aosTest("Keyword Filter1", "Click1", "Expouse1", "aos_htmlap1", 400, 350);
-                aosTest("Keyword Filter2", "Click2", "Expouse2", "aos_htmlap2", 400, 350);
-                aosTest("Keyword Filter3", "Click3", "Expouse3", "aos_htmlap3", 400, 350);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
-    private void aosTest2() throws IOException {
-        // 创建样例数据
-        double[][] datapoints = new double[2][4];
-        datapoints[0][1] = -1;
-        datapoints[1][1] = 1;
-        datapoints[0][2] = 2;
-        datapoints[1][2] = -2;
-        datapoints[0][3] = -3;
-        datapoints[1][3] = 3;
-        datapoints[0][0] = 4;
-        datapoints[1][0] = -4;
-        // 创建数据集
-        DefaultXYDataset dataset = new DefaultXYDataset();
-        dataset.addSeries("Data", datapoints);
-        // 创建 JFreeChart 实例
-        JFreeChart scatterChart = ChartFactory.createScatterPlot("Scatter Plot Demo", "X", "Y", dataset,
-            PlotOrientation.VERTICAL, true, true, false);
-        // 设置散点样式
-        scatterChart.getXYPlot().getRenderer().setSeriesShape(0, new Ellipse2D.Double(-3, -3, 6, 6));
-        scatterChart.getXYPlot().getRenderer().setSeriesPaint(0, Color.RED);
-        // 创建缓冲图像
-        BufferedImage chartImage = new BufferedImage(800, 600, BufferedImage.TYPE_INT_RGB);
-        Graphics2D g2d = chartImage.createGraphics();
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.setBackground(Color.WHITE);
-        g2d.clearRect(0, 0, 800, 600);
-        // 绘制图表
-        scatterChart.draw(g2d, new java.awt.Rectangle(50, 50, 800, 600), null, null);
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        try {
-            ImageIO.write(chartImage, "png", baos);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        byte[] imageBytes = baos.toByteArray();
-        String base64Image = java.util.Base64.getEncoder().encodeToString(imageBytes);
-        // 生成HTML代码
-        String html = "<html><body><img src='data:image/png;base64," + base64Image + "'/></body></html>";
-        System.out.println(html);
-        Html htmlControl = this.getView().getControl("aos_htmlap");
-        htmlControl.setConent(html);
-    }
+        if (AOS_CAL.equals(control)) {
 
-    private void aosTest(String title, String xName, String yName, String sign, int width, int height)
-        throws IOException {
-        DefaultXYDataset dataset = new DefaultXYDataset();
-        // 生成100个随机数据点
-        double[][] data = generateRandomData(100);
-        dataset.addSeries("Sport", data);
-        double[][] data2 = generateRandomData(100);
-        dataset.addSeries("Home", data2);
-        // 创建散点图
-        JFreeChart scatterPlot = ChartFactory.createScatterPlot(
-            // 标题
-            title,
-            // X轴标签
-            xName,
-            // Y轴标签
-            yName,
-            // 数据集
-            dataset);
-        // 将散点图生成为图片，并以Base64编码的方式嵌入到HTML代码中
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        ChartUtilities.writeChartAsPNG(outputStream, scatterPlot, width, height);
-        byte[] imageBytes = outputStream.toByteArray();
-        String base64Image = java.util.Base64.getEncoder().encodeToString(imageBytes);
-        // 生成HTML代码
-        String html = "<html><body><img src='data:image/png;base64," + base64Image + "'/></body></html>";
-        Html htmlControl = this.getView().getControl(sign);
-        htmlControl.setConent(html);
-    }
+        }
 
-    private void aosCal() {
-        TreeView treeView = this.getView().getControl("aos_treeview");
-        treeView.deleteAllNodes();
-        DynamicObjectCollection tmpS = QueryServiceHelper.query("aos_mkt_pop_tmp",
-            "aos_itemid,aos_roi,aos_flow,aos_click," + "aos_itemid.number item_number", null);
-        DynamicObjectCollection entityS = this.getModel().getEntryEntity("aos_entryentity");
-        List<TreeNode> list = new ArrayList<>();
-        List<String[]> total = new ArrayList<>();
-        for (DynamicObject entity : entityS) {
-            DynamicObject standard = entity.getDynamicObject("aos_standard");
-            String cond = entity.getString("aos_cond");
-            BigDecimal value = entity.getBigDecimal("aos_value");
-            String stNamePos = standard.getString("name");
-            String stNameNega = "!" + stNamePos;
-            total.add(new String[] {stNamePos, stNameNega});
-        }
-        List<String[]> cartesianList = cartesianProduct(total);
-        // 输出结果
-        for (String[] arr : cartesianList) {
-            System.out.println(Arrays.toString(arr));
-            String key = String.join("&", arr);
-            list.add(new TreeNode(null, key, key));
-            for (DynamicObject tmp : tmpS) {
-                list.add(new TreeNode(key, key + tmp.getString("item_number"), tmp.getString("item_number")));
-            }
-        }
-        // 赋值数据
-        treeView.setMulti(false);
-        treeView.addNodes(list);
-        treeView.setRootVisible(true);
-        treeView.showNode("head");
     }
 
 }
--- java/mkt/image/test/AosMktTestForm.java
+++ java/mkt/image/test/AosMktTestForm.java
@@ -1,47 +1,50 @@
 package mkt.image.test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.util.*;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.EventObject;
 
-import com.google.gson.*;
 import common.fnd.FndMsg;
-import freemarker.cache.FileTemplateLoader;
-import freemarker.template.TemplateException;
-import kd.bos.form.control.Control;
-import kd.bos.form.control.Html;
+import kd.bos.dataentity.entity.DynamicObjectCollection;
+import kd.bos.form.events.CustomEventArgs;
 import kd.bos.form.control.events.ItemClickEvent;
 import kd.bos.form.plugin.AbstractFormPlugin;
-
-import java.io.IOException;
-
-import freemarker.template.Configuration;
-import freemarker.template.Template;
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartUtilities;
-import org.jfree.chart.JFreeChart;
-import org.jfree.data.xy.DefaultXYDataset;
-
-import java.io.StringWriter;
 
 /**
  * @author aosom
  * @version 测试表单插件
  */
 public class AosMktTestForm extends AbstractFormPlugin {
-    private final long n = 0;
 
-    private static double[][] generateRandomData(int count) {
-        double[][] data = new double[2][count];
-        Random random = new Random();
-        for (int i = 0; i < count; i++) {
-            double x = random.nextDouble();
-            double y = random.nextDouble();
-            data[0][i] = x;
-            data[1][i] = y;
+    public static void outputDynamicRangeCombinations(int[] xValues, int[] yValues) {
+        ArrayList<String> outputList = new ArrayList<>();
+        for (int x : xValues) {
+            for (int y : yValues) {
+                StringBuilder output = new StringBuilder();
+                if (x < xValues[0]) {
+                    output.append("x<=").append(xValues[0]);
+                } else if (x > xValues[1]) {
+                    output.append("x>=").append(xValues[1]);
+                } else {
+                    output.append(xValues[0]).append("<=x<=").append(xValues[1]);
+                }
+                output.append(" and ");
+                if (y < yValues[0]) {
+                    output.append("y<=").append(yValues[0]);
+                } else if (y > yValues[1]) {
+                    output.append("y>=").append(yValues[1]);
+                } else {
+                    output.append(yValues[0]).append("<=y<=").append(yValues[1]);
+                }
+                output.append(" ;");
+                outputList.add(output.toString());
+            }
         }
-        return data;
+        outputList.forEach(System.out::println);
     }
+
+    @Override
+    public void customEvent(CustomEventArgs e) {}
 
     @Override
     public void registerListener(EventObject e) {
@@ -49,115 +52,54 @@ public class AosMktTestForm extends AbstractFormPlugin {
         // 给工具栏加监听事件
         this.addItemClickListeners("aos_toolbarap");
         // 提交
-        this.addItemClickListeners("aos_test");
+        this.addItemClickListeners("aos_cal");
     }
 
     @Override
     public void itemClick(ItemClickEvent evt) {
         super.itemClick(evt);
         String control = evt.getItemKey();
+
         if ("aos_cal".equals(control)) {
-            try {
-                aosTest4();
-            } catch (IOException e) {
-                e.printStackTrace();
-                throw new RuntimeException(e);
-            }
+            generateRanges();
         }
+
     }
 
-    @Override
-    public void afterCreateNewData(EventObject e) {}
+    public void generateRanges() {
+        // 定义待筛选的坐标轴
+        // 点击A
+        BigDecimal xAxis1 = (BigDecimal)this.getModel().getValue("aos_x1");
+        // 点击B
+        BigDecimal xAxis2 = (BigDecimal)this.getModel().getValue("aos_x2");
+        // 曝光A
+        BigDecimal yAxis1 = (BigDecimal)this.getModel().getValue("aos_y1");
+        // 曝光B
+        BigDecimal yAxis2 = (BigDecimal)this.getModel().getValue("aos_y2");
 
-    private void aosTest4() throws IOException {
-        DefaultXYDataset dataset = new DefaultXYDataset();
-        // 生成100个随机数据点
-        double[][] data = generateRandomData(100);
-        dataset.addSeries("Scatter Plot", data);
+        FndMsg.debug("xAxis1:" + xAxis1);
+        FndMsg.debug("xAxis2:" + xAxis2);
+        FndMsg.debug("yAxis1:" + yAxis1);
+        FndMsg.debug("yAxis2:" + yAxis2);
 
-        // 创建散点图
-        JFreeChart scatterPlot = ChartFactory.createScatterPlot(
-            // 标题
-            "Scatter Plot Example",
-            // X轴标签
-            "X",
-            // Y轴标签
-            "Y",
-            // 数据集
-            dataset);
-        // 将散点图生成为图片，并以Base64编码的方式嵌入到HTML代码中
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        ChartUtilities.writeChartAsPNG(outputStream, scatterPlot, 800, 600);
-        byte[] imageBytes = outputStream.toByteArray();
-        String base64Image = java.util.Base64.getEncoder().encodeToString(imageBytes);
+        int[] xValues = {1, 2};
+        int[] yValues = {3, 4};
+        outputDynamicRangeCombinations(xValues, yValues);
 
-        // 生成HTML代码
-        String html = "<html><body><img src='data:image/png;base64," + base64Image + "'/></body></html>";
-        System.out.println(html);
+        // 打印筛选结果
+        DynamicObjectCollection aosEntryentityS = this.getModel().getEntryEntity("aos_entryentity");
+        aosEntryentityS.clear();
+        int i = 0;
+        // for (CoordinateRange coordinateRange : result) {
+        // this.getModel().batchCreateNewEntryRow("aos_entryentity", 1);
+        // this.getModel().setValue("aos_xstart", coordinateRange.getXStart(), i);
+        // this.getModel().setValue("aos_xend", coordinateRange.getXEnd(), i);
+        // this.getModel().setValue("aos_ystart", coordinateRange.getYStart(), i);
+        // this.getModel().setValue("aos_yend", coordinateRange.getYEnd(), i);
+        // i++;
+        // }
 
-        Html htmlControl = this.getView().getControl("aos_htmlap");
-        htmlControl.setConent(html);
-    }
-
-    private void aosTest3() throws IOException, TemplateException {
-        // 创建数据点
-        List<DataPoint> data = new ArrayList<>();
-        data.add(new DataPoint(1, 10));
-        data.add(new DataPoint(2, 20));
-        data.add(new DataPoint(3, 15));
-        data.add(new DataPoint(4, 25));
-        data.add(new DataPoint(5, 18));
-        // 将数据转换为JSON数组
-        Gson gson = new Gson();
-        String json = gson.toJson(data);
-        FndMsg.debug(json);
-        Configuration configuration = new Configuration();
-        FileTemplateLoader templateLoader =
-            new FileTemplateLoader(new File(System.getProperty("user.dir") + "/mms/java/templates"));
-        configuration.setTemplateLoader(templateLoader);
-        freemarker.template.Template template = configuration.getTemplate("chart_template.html");
-        StringWriter writer = new StringWriter();
-        Map<String, Object> dataModel = new HashMap<>(16);
-        dataModel.put("jsonString", json);
-        template.process(dataModel, writer);
-        String html = writer.toString();
-        FndMsg.debug("html:" + html);
-        Html htmlControl = this.getView().getControl("aos_htmlap");
-        htmlControl.setConent(html);
-    }
-
-    public static class DataPoint {
-        private int x;
-        private int y;
-
-        public DataPoint(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        // Getter和Setter方法
-        public int getX() {
-            return x;
-        }
-
-        public void setX(int x) {
-            this.x = x;
-        }
-
-        public int getY() {
-            return y;
-        }
-
-        public void setY(int y) {
-            this.y = y;
-        }
-        // 省略构造函数和getter/setter方法
-
-        // 将数据转换为JSON格式
-        public String toJSON() {
-            Gson gson = new Gson();
-            return gson.toJson(this);
-        }
+        this.getView().updateView();
     }
 
 }
